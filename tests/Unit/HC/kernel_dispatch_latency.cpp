f5464a52715e80f0fa64c970395e07f851cd49c4
Hui <hui@multicorewareinc.com>
2016-11-27 16:17:29 -0600

    Fix two cases using raw pointers that freeze CUDA machine

--- tests/Unit/HC/kernel_dispatch_latency.cpp
+++ tests/Unit/HC/kernel_dispatch_latency.cpp
@@ -1,7 +1,8 @@
 
-// RUN: %hc %s -o %t.out && %t.out
+// RUN: %hc %s -lhc_am -o %t.out && %t.out
 
 #include <hc.hpp>
+#include <hc_am.hpp>
 
 #include <iostream>
 #include <random>
@@ -48,19 +49,17 @@ bool test2() {
   // define inputs and output
   const int vecSize = 2048;
 
-  int table_a[vecSize];
-  int table_b[vecSize];
-  int table_c[vecSize];
-  int *p_a = &table_a[0];
-  int *p_b = &table_b[0];
-  int *p_c = &table_c[0];
+  hc::accelerator acc = hc::accelerator();
+  int *p_a = (int*)hc::am_alloc(vecSize*sizeof(int),acc,1);
+  int *p_b = (int*)hc::am_alloc(vecSize*sizeof(int),acc,1);
+  int *p_c = (int*)hc::am_alloc(vecSize*sizeof(int),acc,1);
 
   // initialize test data
   std::random_device rd;
   std::uniform_int_distribution<int32_t> int_dist;
   for (int i = 0; i < vecSize; ++i) {
-    table_a[i] = int_dist(rd);
-    table_b[i] = int_dist(rd);
+    p_a[i] = int_dist(rd);
+    p_b[i] = int_dist(rd);
   }
 
   long time_spent = 0, time_spent_once;
@@ -78,7 +77,6 @@ bool test2() {
     });
     fut.wait();
     ret &= (fut.is_ready() == true);
-
     clock_gettime(CLOCK_REALTIME, &end);
     time_spent_once = ((end.tv_sec - begin.tv_sec) * 1000 * 1000) + ((end.tv_nsec - begin.tv_nsec) / 1000);
     time_spent += time_spent_once;
@@ -86,6 +84,10 @@ bool test2() {
 
   std::cout << "Dispatched " << DISPATCH_COUNT << " vector addition kernels\n";
   std::cout << "Average dispatch time per kernel: " << ((double)time_spent / DISPATCH_COUNT) << "us\n";
+
+  hc::am_free(p_a);
+  hc::am_free(p_b);
+  hc::am_free(p_c);
 
   return ret;
 }
--- tests/Unit/HC/kernel_enqueue_overhead.cpp
+++ tests/Unit/HC/kernel_enqueue_overhead.cpp
@@ -1,7 +1,8 @@
 
-// RUN: %hc %s -o %t.out && %t.out
+// RUN: %hc %s -lhc_am -o %t.out && %t.out
 
 #include <hc.hpp>
+#include <hc_am.hpp>
 
 #include <iostream>
 #include <random>
@@ -48,19 +49,17 @@ bool test2() {
   // define inputs and output
   const int vecSize = 2048;
 
-  int table_a[vecSize];
-  int table_b[vecSize];
-  int table_c[vecSize];
-  int *p_a = &table_a[0];
-  int *p_b = &table_b[0];
-  int *p_c = &table_c[0];
+  hc::accelerator acc = hc::accelerator();
+  int *p_a = (int*)hc::am_alloc(vecSize*sizeof(int),acc,1);
+  int *p_b = (int*)hc::am_alloc(vecSize*sizeof(int),acc,1);
+  int *p_c = (int*)hc::am_alloc(vecSize*sizeof(int),acc,1);
 
   // initialize test data
   std::random_device rd;
   std::uniform_int_distribution<int32_t> int_dist;
   for (int i = 0; i < vecSize; ++i) {
-    table_a[i] = int_dist(rd);
-    table_b[i] = int_dist(rd);
+    p_a[i] = int_dist(rd);
+    p_b[i] = int_dist(rd);
   }
 
   long time_spent = 0, time_spent_once;
@@ -86,6 +85,10 @@ bool test2() {
 
   std::cout << "Enqueued " << DISPATCH_COUNT << " vector addition kernels\n";
   std::cout << "Average time per kernel: " << ((double)time_spent / DISPATCH_COUNT) << "us\n";
+
+  hc::am_free(p_a);
+  hc::am_free(p_b);
+  hc::am_free(p_c);
 
   return ret;
 }
